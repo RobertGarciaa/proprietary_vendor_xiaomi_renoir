d2a0a88050150bddebac4985ab78050c7a11adb2
Arian <arian.kulmer@web.de>
2024-05-20 18:41:09 +0200

    renoir: Import firmware from V14.0.7.0.TKIMIXM
    Change-Id: I7d227e7c52ac46e9973061e7553b34c373b55f44

--- Android.mk
+++ Android.mk
@@ -6,4 +6,22 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),renoir)
 
+$(call add-radio-file-sha1-checked,radio/abl.img,8263504cda85ca1bb4fb1c3c345b22c49919f803)
+$(call add-radio-file-sha1-checked,radio/aop.img,53f3f005ef08b0904b86fdd6f04b3e5bb95b4c1a)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,433c7612bb59f4c7b161d997ff5e39c8c2b23aaf)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,6fce054f701a8b8583fcee474618c506acd7e092)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,695e9559ce861138671e9a4d487707b6e0feffd4)
+$(call add-radio-file-sha1-checked,radio/dsp.img,613904995e6bf2cb25605e84c96817505cc44b2e)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,d76d8876a8fa7aa692c7bc1e07435b339016262e)
+$(call add-radio-file-sha1-checked,radio/hyp.img,f9d2a997b1d7ef81458621f7ed2dfd9aa9a6fecf)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,c1982af91c84a0815279991d6b15f478e4285fb2)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,47d95e4c13b0639c0d56e5793a18d3707ffacf4c)
+$(call add-radio-file-sha1-checked,radio/modem.img,6ade64ba894ce3c116fb18adc24c473d1eda5b38)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,e84e6d318e3392d241aa36e27ed4644704e00f72)
+$(call add-radio-file-sha1-checked,radio/shrm.img,5058f6711050b3d048ce69e587ee20ffdeaeec61)
+$(call add-radio-file-sha1-checked,radio/tz.img,1a70b67054f357b13aed31e1e08abb602ce78d7f)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,5772c2b8ffdb2c6ad7a27e053faa297df893c9d8)
+$(call add-radio-file-sha1-checked,radio/xbl.img,8a17f5445becf812fe909d4285a996121f137b27)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,1a6f484c3edacac44209289b5d70e92ee4f3527c)
+
 endif
